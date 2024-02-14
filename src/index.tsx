3b3f7e28ea3a628202e5cf16a9d1d56a23229eff
Julian Feregotti <julianferegotti96@gmail.com>
2024-02-13 22:02:55 -0300

    Add useDocumentTitle hook and update useQueryParams hook

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,13 @@ import useDebounce from "./useDebounce";
 import useThrottle from "./useThrottle";
 import useLocalStorage from "./useLocalStorage";
 import useDelay from "./useDelay";
+import useDocumentTitle from "./useDocumentTitle";
 
-export { useAsync, useDebounce, useThrottle, useLocalStorage, useDelay };
+export {
+  useAsync,
+  useDebounce,
+  useThrottle,
+  useLocalStorage,
+  useDelay,
+  useDocumentTitle,
+};
